8a25291a38d6def2f0913b00840c73b02c346b0e
Aiza Tilenbaeva <tilenbaevaa@gmail.com>
2019-03-24 16:03:05 -0500

    new muiltiple properties

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 node{
-    properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please, specify IP address', name: 'IP', trim: true)])])
+    properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please, give IP for container to build', name: 'IP', trim: true), string(defaultValue: 'latest', description: 'What version would like to you to deploy?', name: 'VER', trim: true)])])
     stage ("Remove container"){
         try{
             sh "ssh root@${IP} docker rm -f Flaskex"
@@ -11,6 +11,6 @@ node{
 
 
     stage("Run container"){
-        sh "ssh root@${IP} docker run -d --name Flaskex -p 4400:4000 tilenbaevaa/flaskex "
+        sh "ssh root@${IP} docker run -d --name Flaskex -p 4400:4000 tilenbaevaa/flaskex_centos7 "
     }
 }
